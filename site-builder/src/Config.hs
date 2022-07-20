ae501aa836d1030ecd5c65c3f46cf85634c26d53
Ashley Smith <git@aas.sh>
2022-07-20 19:48:05 +0100

    Made it possible to write math inside posts

--- site-builder/src/Config.hs
+++ site-builder/src/Config.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module Config 
+module Config
 ( sourceDir
 , domain
 , root
@@ -25,8 +25,8 @@ module Config
 import Hakyll
 import Data.List (isPrefixOf)
 import System.FilePath.Posix (takeFileName)
-import Text.Pandoc (ReaderOptions (readerExtensions), def, enableExtension)
-import Text.Pandoc.Options (Extension (Ext_citations))
+import Text.Pandoc (ReaderOptions (readerExtensions), def, extensionsFromList, Extensions)
+import Text.Pandoc.Options (Extension(..))
 
 -- Site configuration
 --------------------------------------------------------------------------------
@@ -44,13 +44,13 @@ root = "https://" ++ domain
 --------------------------------------------------------------------------------
 
 hakyllConfig :: Configuration
-hakyllConfig = defaultConfiguration 
+hakyllConfig = defaultConfiguration
   { destinationDirectory = "build/site"
   , storeDirectory       = "build/_store"
   , tmpDirectory         = "build/_tmp"
   , providerDirectory    = sourceDir
   , ignoreFile           = ignoreFile'
-  } 
+  }
   where ignoreFile' path
          | "."`isPrefixOf` fileName = True
          | otherwise = False
@@ -112,5 +112,67 @@ svg = "**.svg"
 
 readerOptions:: ReaderOptions
 readerOptions = def {
-  readerExtensions = enableExtension Ext_citations (readerExtensions defaultHakyllReaderOptions)
+  readerExtensions = siteReaderExtensions
 }
+
+
+siteReaderExtensions :: Extensions
+siteReaderExtensions = extensionsFromList
+    -- Site extensions
+  [ Ext_citations
+  , Ext_emoji
+  , Ext_latex_macros
+  , Ext_tex_math_dollars
+  , Ext_tex_math_double_backslash
+
+  -- Default hakyll reader extensions
+  , Ext_smart
+
+  -- Pandoc extensions
+  , Ext_footnotes
+  , Ext_inline_notes
+  , Ext_pandoc_title_block
+  , Ext_yaml_metadata_block
+  , Ext_table_captions
+  , Ext_implicit_figures
+  , Ext_simple_tables
+  , Ext_multiline_tables
+  , Ext_grid_tables
+  , Ext_pipe_tables
+  , Ext_citations
+  , Ext_raw_tex
+  , Ext_raw_html
+  , Ext_tex_math_dollars
+  , Ext_latex_macros
+  , Ext_fenced_code_blocks
+  , Ext_fenced_code_attributes
+  , Ext_backtick_code_blocks
+  , Ext_inline_code_attributes
+  , Ext_raw_attribute
+  , Ext_markdown_in_html_blocks
+  , Ext_native_divs
+  , Ext_fenced_divs
+  , Ext_native_spans
+  , Ext_bracketed_spans
+  , Ext_escaped_line_breaks
+  , Ext_fancy_lists
+  , Ext_startnum
+  , Ext_definition_lists
+  , Ext_example_lists
+  , Ext_all_symbols_escapable
+  , Ext_intraword_underscores
+  , Ext_blank_before_blockquote
+  , Ext_blank_before_header
+  , Ext_space_in_atx_header
+  , Ext_strikeout
+  , Ext_superscript
+  , Ext_subscript
+  , Ext_task_lists
+  , Ext_auto_identifiers
+  , Ext_header_attributes
+  , Ext_link_attributes
+  , Ext_implicit_header_references
+  , Ext_line_blocks
+  , Ext_shortcut_reference_links
+  , Ext_smart
+  ]
