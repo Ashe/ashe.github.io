6c5f529f6fec933466fc168752351dc9f10411a9
Ashley Smith <git@aas.sh>
2022-07-21 21:18:16 +0100

    Publish site from 7bd3fd2

---
+++ assets/scripts/init-scrollspy.js
@@ -0,0 +1,9 @@
+// Navbar table-of-contents
+const nav_toc = document.querySelector('#nav-toc');
+const nav_toc_scrollspy = VanillaScrollspy(nav_toc);
+nav_toc_scrollspy.init();
+
+// Sidebar table-of-contents
+const side_toc = document.querySelector('#side-toc');
+const side_toc_scrollspy = VanillaScrollspy(side_toc);
+side_toc_scrollspy.init();
