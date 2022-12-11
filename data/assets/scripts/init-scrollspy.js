5537459557a064e5bdb306de899c84053887fa88
Ashley Smith <git@aas.sh>
2022-12-11 22:37:10 +0000

    Fixed vanillajs-scrollspy usage

--- data/assets/scripts/init-scrollspy.js
+++ data/assets/scripts/init-scrollspy.js
@@ -1,9 +1,13 @@
-// Navbar table-of-contents
-const nav_toc = document.querySelector('#nav-toc');
-const nav_toc_scrollspy = VanillaScrollspy(nav_toc);
-nav_toc_scrollspy.init();
+{
+  // Navbar table-of-contents
+  const menu = document.querySelector('#nav-toc');
+  const scrollspy = VanillaScrollspy({menu});
+  scrollspy.init();
+}
 
-// Sidebar table-of-contents
-const side_toc = document.querySelector('#side-toc');
-const side_toc_scrollspy = VanillaScrollspy(side_toc);
-side_toc_scrollspy.init();
+{
+  // Sidebar table-of-contents
+  const menu = document.querySelector('#side-toc');
+  const scrollspy = VanillaScrollspy({menu});
+  scrollspy.init();
+}
